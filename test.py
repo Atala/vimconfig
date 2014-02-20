5c5786ae6a8257325f62ce136943675384d29dd7
Guillope <alois.guillope@gmail.com>
2014-02-20 09:44:33 +0100

    initial commit

---
+++ test.py
@@ -0,0 +1,10 @@
+from django import *;arg
+
+classarg lol(object):
+	"""amha"""
+	def __init__(self, request, **kwargs):	super(lol, self).__init__()
+def lol(sqdsqdj):
+	"""sqklslq"""
+	return x**2
+
+from django.conf.url import sqdsqdj
